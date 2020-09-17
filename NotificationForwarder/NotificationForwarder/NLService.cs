89c2dcac50cada24f88615e0f86ccbd46b59cc48
Chris Grabosky <chris@grabosky.net>
2020-09-17 11:43:00 -0400

    initial commit

---
+++ NotificationForwarder/NotificationForwarder/NLService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Service.Notification;
+using Android.Views;
+using Android.Widget;
+
+namespace NotificationForwarder
+{
+    [Service(Label ="MyNotificationListenerService", Permission = "android.permission.BIND_NOTIFICATION_LISTENER_SERVICE")]
+    [IntentFilter(new[] { "android.service.notification.NotificationListenerService"})]
+    class NLService: NotificationListenerService
+    {
+        public override void OnNotificationPosted(StatusBarNotification sbn)
+        {
+            base.OnNotificationPosted(sbn);
+            string pn = sbn.PackageName;
+        }
+    }
+}
