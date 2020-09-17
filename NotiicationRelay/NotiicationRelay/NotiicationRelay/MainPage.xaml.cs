6910d6bd386304c95544d48da8d0197f546dfd11
Chris Grabosky <chris@grabosky.net>
2020-09-17 15:07:19 -0400

    moved to xamarin forms

---
+++ NotiicationRelay/NotiicationRelay/NotiicationRelay/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NotiicationRelay
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+
+    public partial class MainPage : ContentPage
+    {
+        NLService nls = new NLService();
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            lbl_main.Text = string.Join(",",nls.sbnList.ToArray());
+        }
+    }
+}
---
+++ NotiicationRelay/NotiicationRelay/NotiicationRelay/NLService.cs
@@ -0,0 +1,28 @@
+﻿using Android.Service.Notification;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Android.App;
+using Android.Content;
+
+namespace NotiicationRelay
+{
+    [Service(Label = "MyNotificationListenerService", Permission = "android.permission.BIND_NOTIFICATION_LISTENER_SERVICE")]
+    [IntentFilter(new[] { "android.service.notification.NotificationListenerService" })]
+    class NLService : NotificationListenerService
+    {
+        public Queue<string> sbnList = new Queue<string>();
+
+        public override void OnNotificationPosted(StatusBarNotification sbn)
+        {
+
+            sbnList.Enqueue(sbn.PackageName);
+            if (sbnList.Count > 5)
+            {
+                sbnList.Dequeue();
+            }
+            base.OnNotificationPosted(sbn);
+
+        }
+    }
+}
